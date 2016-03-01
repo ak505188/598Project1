cf64c016ae4001a7ca8b4963b8994905dffef306
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-01 14:57:44 -0800

    Converted tabs to spaces.

--- Catapult.java
+++ Catapult.java
@@ -10,18 +10,18 @@ import lejos.hardware.motor.Motor;
 
 public class Catapult {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    /*		
-		// This assumes that the Sensor is plugged into the first Sensor Port
-		Port p1 = LocalEV3.get().getPort("S1");
-		// Create a ultrasonic object using p1
-		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(p1);
-		// EV3 sensors use the sample provider class to store their data
-		SampleProvider sample = sensor.getMode("Distance");
-		// sample provider stores data in a float. This gets the size the float
-		// needs to be
-		float[] distance = new float[sample.sampleSize()];
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    /*    
+    // This assumes that the Sensor is plugged into the first Sensor Port
+    Port p1 = LocalEV3.get().getPort("S1");
+    // Create a ultrasonic object using p1
+    EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(p1);
+    // EV3 sensors use the sample provider class to store their data
+    SampleProvider sample = sensor.getMode("Distance");
+    // sample provider stores data in a float. This gets the size the float
+    // needs to be
+    float[] distance = new float[sample.sampleSize()];
     */
     // Set Speed
 
@@ -30,36 +30,36 @@ public class Catapult {
     SampleProvider distSample = distSensor.getDistanceMode();
     float[] distance = new float[1];
 
-  	Motor.B.forward();
-		while(true) {
-			distSample.fetchSample(distance, 0);
+    Motor.B.forward();
+    while(true) {
+      distSample.fetchSample(distance, 0);
       System.out.println("DistSample SampleSize: " + distSample.sampleSize());
       System.out.println("Distance: " + distance[0]);
-			//This gets the sensor data and stores it in the distance float
-			//The distance data is stored in index 0 of the array
-			//If there is an object within 2 CM (Meters is default measurement) 
-			if (distance[0] < .05) {
-			  // Fire catapult!
-				launch();
-			}
+      //This gets the sensor data and stores it in the distance float
+      //The distance data is stored in index 0 of the array
+      //If there is an object within 2 CM (Meters is default measurement) 
+      if (distance[0] < .05) {
+        // Fire catapult!
+        launch();
+      }
       if (Button.ESCAPE.isDown()) {
         Motor.B.stop();
         distSensor.close();
         System.exit(1);
-			}
-		}
-
-	}
-	
-	public static void launch()
-	{
-		Motor.B.stop();
-		Motor.A.setSpeed(Motor.A.getMaxSpeed());
-
-    for (int i = 0; i < 3; i++) {	
-		  Motor.A.rotateTo(-95);
-		  Motor.A.rotateTo(0);
+      }
     }
-    Motor.B.forward();	
-	}
+
+  }
+  
+  public static void launch()
+  {
+    Motor.B.stop();
+    Motor.A.setSpeed(Motor.A.getMaxSpeed());
+
+    for (int i = 0; i < 3; i++) {  
+      Motor.A.rotateTo(-95);
+      Motor.A.rotateTo(0);
+    }
+    Motor.B.forward();  
+  }
 }
