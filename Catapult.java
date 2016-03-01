eeb230765e98b0a69d74702c6ab67179468fe2a9
WTFranklin <faddelia@gmail.com>
2016-03-01 13:34:10 -0800

    Catapult.java file

--- Catapult.java
+++ Catapult.java
@@ -1,12 +1,27 @@
 package catapult;
 
 import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
 import lejos.hardware.motor.Motor;
 
 public class Catapult {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// This assumes that the Sensor is plugged into the first Sensor Port
+		Port p1 = LocalEV3.get().getPort("S1");
+		// Create a ultrasonic object using p1
+		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(p1);
+		// EV3 sensors use the sample provider class to store their data
+		SampleProvider sample = sensor.getMode("Distance");
+		// sample provider stores data in a float. This gets the size the float
+		// needs to be
+		float[] distance = new float[sample.sampleSize()];
 		
 		while(true)
 		{
@@ -15,7 +30,28 @@ public class Catapult {
 			switch(press)
 			{
 				case (Button.ID_ENTER):
-					launch();
+					
+					boolean exit = false;
+					while(!exit)
+					{
+						Motor.B.forward();
+						//This gets the sensor data and stores it in the distance float
+						sample.fetchSample(distance, 0);
+						//The distance data is stored in index 0 of the array
+						//If there is an object within 2 CM (Meters is default measurement) 
+						if (distance[0] < .02)
+						{
+							//Fire catapult!
+							Motor.B.stop();
+							launch();
+						}
+						press = Button.waitForAnyEvent(3000);
+						
+						if(press != Button.ID_ENTER)
+						{
+							exit = true;
+						}
+					}
 					break;
 				case (Button.ID_ESCAPE):
 					System.exit(0);
@@ -30,7 +66,7 @@ public class Catapult {
 		boolean launched = false;
 		
 		Motor.A.setSpeed(Motor.A.getMaxSpeed());
-		Motor.A.rotateTo(-75);
+		Motor.A.rotateTo(-95);
 		
 		Motor.A.rotateTo(0);
 		
