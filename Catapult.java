01301760e44ae4f4d2e698d3f1c650f3f6b141e5
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-01 14:11:05 -0800

    Cleaned up catapult.

--- Catapult.java
+++ Catapult.java
@@ -22,55 +22,37 @@ public class Catapult {
 		// sample provider stores data in a float. This gets the size the float
 		// needs to be
 		float[] distance = new float[sample.sampleSize()];
-		
-		while(true)
-		{
-			int press = Button.waitForAnyPress();
-			
-			switch(press)
-			{
-				case (Button.ID_ENTER):
-					
-					boolean exit = false;
-					while(!exit)
-					{
-						Motor.B.forward();
-						//This gets the sensor data and stores it in the distance float
-						sample.fetchSample(distance, 0);
-						//The distance data is stored in index 0 of the array
-						//If there is an object within 2 CM (Meters is default measurement) 
-						if (distance[0] < .02)
-						{
-							//Fire catapult!
-							Motor.B.stop();
-							launch();
-						}
-						press = Button.waitForAnyEvent(3000);
-						
-						if(press != Button.ID_ENTER)
-						{
-							exit = true;
-						}
-					}
-					break;
-				case (Button.ID_ESCAPE):
-					System.exit(0);
-					break;
+
+    // Set Speed
+
+		Motor.B.forward();
+		while(true) {
+			sample.fetchSample(distance, 0);
+			//This gets the sensor data and stores it in the distance float
+			//The distance data is stored in index 0 of the array
+			//If there is an object within 2 CM (Meters is default measurement) 
+			if (distance[0] < .02) {
+			  // Fire catapult!
+				launch();
+			}
+      if (Button.ESCAPE.isDown()) {
+        Motor.B.stop();
+        sensor.close();
+        System.exit(1);
 			}
 		}
 
 	}
 	
-	public static boolean launch()
+	public static void launch()
 	{
-		boolean launched = false;
-		
+		Motor.B.stop();
 		Motor.A.setSpeed(Motor.A.getMaxSpeed());
-		Motor.A.rotateTo(-95);
-		
-		Motor.A.rotateTo(0);
-		
-		return launched;
-	}
 
+    for (int i = 0; i < 3; i++) {	
+		  Motor.A.rotateTo(-95);
+		  Motor.A.rotateTo(0);
+    }
+    Motor.B.forward();	
+	}
 }
